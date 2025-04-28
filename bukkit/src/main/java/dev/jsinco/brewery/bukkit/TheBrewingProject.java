c5c00060092e6c1f10e0bc3363857a960a7a56cf
Nadwey <nadweyyy@gmail.com>
2025-04-28 23:38:39 +0200

    feat(integration): Overengineered integration system

--- bukkit/src/main/java/dev/jsinco/brewery/bukkit/TheBrewingProject.java
+++ bukkit/src/main/java/dev/jsinco/brewery/bukkit/TheBrewingProject.java
@@ -15,8 +15,8 @@ import dev.jsinco.brewery.bukkit.effect.SqlDrunkStateDataType;
 import dev.jsinco.brewery.bukkit.effect.event.CustomDrunkEventReader;
 import dev.jsinco.brewery.bukkit.effect.event.DrunkEventExecutor;
 import dev.jsinco.brewery.bukkit.ingredient.BukkitIngredientManager;
+import dev.jsinco.brewery.bukkit.integration.IntegrationManager;
 import dev.jsinco.brewery.bukkit.integration.item.ChestShopHook;
-import dev.jsinco.brewery.bukkit.integration.structure.StructureAccessHook;
 import dev.jsinco.brewery.bukkit.listeners.BlockEventListener;
 import dev.jsinco.brewery.bukkit.listeners.InventoryEventListener;
 import dev.jsinco.brewery.bukkit.listeners.PlayerEventListener;
@@ -86,6 +86,8 @@ public class TheBrewingProject extends JavaPlugin implements TheBrewingProjectAp
     private long time;
     @Getter
     private BrewManager<ItemStack> brewManager = new BukkitBrewManager();
+    @Getter
+    private final IntegrationManager integrationManager = new IntegrationManager();
 
     @Override
     public void onLoad() {
@@ -193,7 +195,7 @@ public class TheBrewingProject extends JavaPlugin implements TheBrewingProjectAp
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        StructureAccessHook.initiate(this);
+        integrationManager.init();
         ChestShopHook.initiate(this);
         Bukkit.getServicesManager().register(TheBrewingProjectApi.class, this, this, ServicePriority.Normal);
     }
