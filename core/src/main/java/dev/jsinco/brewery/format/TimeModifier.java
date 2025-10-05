71fe49a092bc5f4811243bf700075f60fef3da39
Mitality <96748385+Mitality@users.noreply.github.com>
2025-10-05 03:32:25 +0200

    fix(time-formatting): Fix aging values being displayed incorrectly

--- core/src/main/java/dev/jsinco/brewery/format/TimeModifier.java
+++ core/src/main/java/dev/jsinco/brewery/format/TimeModifier.java
@@ -1,19 +1,18 @@
 package dev.jsinco.brewery.format;
 
 import dev.jsinco.brewery.configuration.Config;
+import lombok.Getter;
 
+@Getter
 public enum TimeModifier {
 
-    NORMAL(1200),
-    COOKING(Config.config().cauldrons().cookingMinuteTicks()),
-    AGING(Config.config().barrels().agingYearTicks() / (365 * 24 * 60));
+    NORMAL(1200d),
+    COOKING((double) Config.config().cauldrons().cookingMinuteTicks()),
+    AGING((double) Config.config().barrels().agingYearTicks() / (365 * 24 * 60));
 
-    private final long ticksPerMinute;
-    TimeModifier(long tpm) {
+    private final double ticksPerMinute; // type intentional
+    TimeModifier(double tpm) {
         this.ticksPerMinute = tpm;
     }
 
-    public double getTicksPerMinute() {
-        return ticksPerMinute;
-    }
 }
