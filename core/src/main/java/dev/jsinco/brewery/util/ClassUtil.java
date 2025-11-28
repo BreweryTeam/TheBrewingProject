ccf5066f8088e3a35a0f4327303022c5075c8e16
Hjalmar Gunnarsson <officialhjalmar.gunnarsson@outlook.com>
2025-11-28 17:38:48 +0100

    fix(integration): Update nexo API

--- core/src/main/java/dev/jsinco/brewery/util/ClassUtil.java
+++ core/src/main/java/dev/jsinco/brewery/util/ClassUtil.java
@@ -1,5 +1,9 @@
 package dev.jsinco.brewery.util;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Optional;
+
 public class ClassUtil {
 
     private ClassUtil() {
@@ -12,6 +16,26 @@ public class ClassUtil {
             return true;
         } catch (ClassNotFoundException e) {
             return false;
+        }
+    }
+
+    public static boolean exists(String className, String methodName, Class<?>... parameterTypes) {
+        try {
+            Class<?> clazz = Class.forName(className);
+            clazz.getMethod(methodName, parameterTypes);
+            return true;
+        } catch (ClassNotFoundException | NoSuchMethodException e) {
+            return false;
+        }
+    }
+
+    public static <T> Optional<T> invoke(Object object, String methodName) {
+        try {
+            Class<?> clazz = object.getClass();
+            Method method = clazz.getMethod(methodName);
+            return Optional.of((T) method.invoke(object));
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            return Optional.empty();
         }
     }
 }
