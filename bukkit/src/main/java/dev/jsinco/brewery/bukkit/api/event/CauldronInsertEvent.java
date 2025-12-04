c91b728e07d88974c881c5850fb19bb07048ab00
Hjalmar Gunnarsson <officialhjalmar.gunnarsson@outlook.com>
2025-12-04 09:51:04 +0100

    fix(brewery-events): Rework events

--- bukkit/src/main/java/dev/jsinco/brewery/bukkit/api/event/CauldronInsertEvent.java
+++ bukkit/src/main/java/dev/jsinco/brewery/bukkit/api/event/CauldronInsertEvent.java
@@ -1,31 +1,26 @@
 package dev.jsinco.brewery.bukkit.api.event;
 
-import dev.jsinco.brewery.api.brew.Brew;
+import dev.jsinco.brewery.bukkit.api.transaction.ItemSource;
+import dev.jsinco.brewery.bukkit.api.transaction.ItemTransactionSession;
 import dev.jsinco.brewery.bukkit.breweries.BukkitCauldron;
 import lombok.Getter;
-import lombok.Setter;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
-import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class CauldronInsertEvent extends PermissibleBreweryEvent implements BrewModifiableEvent {
+public class CauldronInsertEvent extends PermissibleBreweryEvent implements ItemTransactionEvent<ItemSource.ItemBasedSource> {
 
     private static final HandlerList HANDLERS = new HandlerList();
 
     @Getter
     private final BukkitCauldron cauldron;
     @Getter
-    @Setter
-    private Brew brew;
-    @Getter
-    private final ItemStack itemRepresentation;
+    private final ItemTransactionSession<ItemSource.ItemBasedSource> transactionSession;
 
-    public CauldronInsertEvent(BukkitCauldron cauldron, Brew brew, ItemStack itemStack, @Nullable Player player) {
+    public CauldronInsertEvent(BukkitCauldron cauldron, ItemTransactionSession<ItemSource.ItemBasedSource> transactionSession, @Nullable Player player) {
         this.cauldron = cauldron;
-        this.brew = brew;
-        this.itemRepresentation = itemStack;
+        this.transactionSession = transactionSession;
     }
 
     @Override
--- bukkit/src/main/java/dev/jsinco/brewery/bukkit/api/event/DistilleryExtractEvent.java
+++ bukkit/src/main/java/dev/jsinco/brewery/bukkit/api/event/DistilleryExtractEvent.java
@@ -1,33 +1,28 @@
 package dev.jsinco.brewery.bukkit.api.event;
 
-import dev.jsinco.brewery.api.brew.Brew;
+import dev.jsinco.brewery.bukkit.api.transaction.ItemSource;
+import dev.jsinco.brewery.bukkit.api.transaction.ItemTransactionSession;
 import dev.jsinco.brewery.bukkit.breweries.distillery.BukkitDistillery;
 import lombok.Getter;
-import lombok.Setter;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
-import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class DistilleryExtractEvent extends PermissibleBreweryEvent implements ItemModifiableEvent {
+public class DistilleryExtractEvent extends PermissibleBreweryEvent implements ItemTransactionEvent<ItemSource.ItemBasedSource> {
 
     private static final HandlerList HANDLERS = new HandlerList();
     @Getter
     private final BukkitDistillery distillery;
     @Getter
-    private final Brew brew;
+    private final ItemTransactionSession<ItemSource.ItemBasedSource> transactionSession;
     @Getter
     private final @Nullable Player player;
-    @Getter
-    @Setter
-    private ItemStack itemRepresentation;
 
 
-    public DistilleryExtractEvent(BukkitDistillery distillery, Brew brew, ItemStack itemStack, @Nullable Player player) {
+    public DistilleryExtractEvent(BukkitDistillery distillery, ItemTransactionSession<ItemSource.ItemBasedSource> transactionSession, @Nullable Player player) {
         this.distillery = distillery;
-        this.brew = brew;
-        this.itemRepresentation = itemStack;
+        this.transactionSession = transactionSession;
         this.player = player;
     }
 
