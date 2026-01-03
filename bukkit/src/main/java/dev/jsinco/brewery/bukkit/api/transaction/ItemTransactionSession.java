2f681ccd20ba1ef9d38eb2a77e87e77afc06d512
Tisawesomeness <tis_dev@protonmail.com>
2026-01-03 17:41:49 -0500

    fix(brew-events): Clone items on API boundary

--- bukkit/src/main/java/dev/jsinco/brewery/bukkit/api/transaction/ItemTransactionSession.java
+++ bukkit/src/main/java/dev/jsinco/brewery/bukkit/api/transaction/ItemTransactionSession.java
@@ -2,7 +2,7 @@ package dev.jsinco.brewery.bukkit.api.transaction;
 
 import com.google.common.base.Preconditions;
 import lombok.Getter;
-import lombok.Setter;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.Nullable;
 
 public class ItemTransactionSession<T extends ItemSource> {
@@ -16,13 +16,23 @@ public class ItemTransactionSession<T extends ItemSource> {
     /**
      * The item in the transaction
      */
-    @Setter
     @Getter
     private @Nullable T result;
 
 
     public ItemTransactionSession(ItemTransaction transaction, @Nullable T result) {
         this.transaction = Preconditions.checkNotNull(transaction);
-        this.result = result;
+        setResult(result);
+    }
+
+    @SuppressWarnings("unchecked")
+    public void setResult(@Nullable T result) {
+        if (result instanceof ItemSource.ItemBasedSource(ItemStack itemStack)) {
+            // Since result: T is instanceof ItemBasedSource and ItemBasedSource is final,
+            // ItemBasedSource must be T
+            this.result = (T) new ItemSource.ItemBasedSource(itemStack.clone());
+        } else {
+            this.result = result;
+        }
     }
 }
