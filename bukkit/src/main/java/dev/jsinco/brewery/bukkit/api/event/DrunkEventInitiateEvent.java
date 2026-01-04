ec50c3966727bf2fea747a87a86837f20ff7f343
Hjalmar Gunnarsson <officialhjalmar.gunnarsson@outlook.com>
2026-01-04 20:23:16 +0100

    feat(events-api): Add drunken event initiate event

--- bukkit/src/main/java/dev/jsinco/brewery/bukkit/api/event/DrunkEventInitiateEvent.java
+++ bukkit/src/main/java/dev/jsinco/brewery/bukkit/api/event/DrunkEventInitiateEvent.java
@@ -3,10 +3,12 @@ package dev.jsinco.brewery.bukkit.api.event;
 import dev.jsinco.brewery.api.event.DrunkEvent;
 import lombok.Getter;
 import lombok.Setter;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class DrunkEventInitiateEvent extends Event implements Cancellable {
     private static final HandlerList HANDLERS = new HandlerList();
@@ -16,9 +18,12 @@ public class DrunkEventInitiateEvent extends Event implements Cancellable {
     @Getter
     @Setter
     private boolean cancelled;
+    @Getter
+    private final @Nullable Player player;
 
-    public DrunkEventInitiateEvent(DrunkEvent event) {
+    public DrunkEventInitiateEvent(DrunkEvent event, Player player) {
         this.drunkenEvent = event;
+        this.player = player;
     }
 
     @Override
