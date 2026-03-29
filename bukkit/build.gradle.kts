78e0a28409bca8a071c0a80307f92ca81701a46c
Hjalmar Gunnarsson <officialhjalmar.gunnarsson@outlook.com>
2026-03-29 20:18:09 +0200

    fix(typo): remove unused property from defaults

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -103,7 +103,7 @@ tasks {
         minecraftVersion(project.findProperty("minecraft.version")!! as String)
         if (project.findProperty("testing.integrations")!! == "true") {
             downloadPlugins {
-                modrinth("worldedit", "3ISh7ADm")
+                modrinth("worldedit", "JUWRHdru")
                 modrinth("craftengine", "0.0.67")
                 modrinth("bolt", "1f2gAAFO")
                 url("https://api.spiget.org/v2/resources/62325/download")
