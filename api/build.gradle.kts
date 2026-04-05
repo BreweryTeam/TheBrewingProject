8ef5f0b46ec882fe7d9fadda270aff712b528fef
Hjalmar Gunnarsson <officialhjalmar.gunnarsson@outlook.com>
2026-04-05 08:02:42 +0200

    ci(publishing): Change artifact id for api module

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
 publishing {
     publications {
         create<MavenPublication>("maven") {
-            artifactId = "thebrewingproject-bukkit"
+            artifactId = "thebrewingproject-api"
             artifact(tasks["jar"])
         }
     }
