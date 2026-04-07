6e905faf4f9c437e4c69c855e4c2118e44d2da77
Hjalmar Gunnarsson <officialhjalmar.gunnarsson@outlook.com>
2026-04-07 09:26:35 +0200

    fix(barrel-type): Add post validation

--- core/src/main/java/dev/jsinco/brewery/configuration/structure/BarrelTypeDefinition.java
+++ core/src/main/java/dev/jsinco/brewery/configuration/structure/BarrelTypeDefinition.java
@@ -3,10 +3,13 @@ package dev.jsinco.brewery.configuration.structure;
 import dev.jsinco.brewery.api.breweries.BarrelType;
 import dev.jsinco.brewery.api.breweries.BarrelTypeProvider;
 import dev.jsinco.brewery.api.util.BreweryKey;
+import dev.jsinco.brewery.api.util.Logger;
 import eu.okaeri.configs.OkaeriConfig;
 import eu.okaeri.configs.annotation.CustomKey;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public class BarrelTypeDefinition extends OkaeriConfig {
 
@@ -19,10 +22,39 @@ public class BarrelTypeDefinition extends OkaeriConfig {
     @CustomKey("default-proximity-multipliers")
     private double defaultProximity = 0.7D;
 
-    public BarrelType toBarrelType() {
+    public Optional<BarrelType> toBarrelType() {
+        if (name == null) {
+            return Optional.empty();
+        }
         BarrelType.Builder builder = BarrelTypeProvider.builderStatic(name);
-        proximityMultipliers.forEach((key, value) -> builder.addProximity(BreweryKey.parse(key), value));
-        return builder.defaultProximity(defaultProximity)
-                .build();
+        proximityMultipliers.entrySet().stream()
+                .filter(entry -> inBounds(entry.getValue()))
+                .forEach(entry -> builder.addProximity(BreweryKey.parse(entry.getKey()), entry.getValue()));
+        return Optional.of(
+                builder.defaultProximity(inBounds(defaultProximity) ? defaultProximity : 1D)
+                        .build()
+        );
+    }
+
+    public void postValidate(List<BarrelType> barrelTypes) {
+        if (name == null) {
+            Logger.logErr("Barrel type name was null, skipping...");
+        }
+        for (Map.Entry<String, Double> entry : proximityMultipliers.entrySet()) {
+            BreweryKey key = BreweryKey.parse(entry.getKey());
+            if (barrelTypes.stream().map(BarrelType::key).noneMatch(key::equals)) {
+                Logger.logWarn("Invalid barrel type '" + name + "' unknown proximity multiplier barrel type: " + key.minimalized());
+            }
+            if (!inBounds(entry.getValue())) {
+                Logger.logWarn("Invalid barrel type '" + name + "', wrong proximity multiplier number out of range [0, 1]: " + key.minimalized());
+            }
+        }
+        if (!inBounds(defaultProximity)) {
+            Logger.logWarn("Invalid barrel type, default proximity multiplier out of range [0, 1]: " + name);
+        }
+    }
+
+    private boolean inBounds(double value) {
+        return value >= 0 && value <= 1;
     }
 }
