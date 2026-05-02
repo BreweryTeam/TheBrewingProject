009747b78c3aeaac4c2fbad301e8b0ae0621bb23
Hjalmar Gunnarsson <officialhjalmar.gunnarsson@outlook.com>
2026-05-02 10:43:31 +0200

    feat(integration): Add all events + endure pain

--- bukkit-impl/src/main/java/dev/jsinco/brewery/bukkit/integration/event/skript/TbpSkriptEventStructure.java
+++ bukkit-impl/src/main/java/dev/jsinco/brewery/bukkit/integration/event/skript/TbpSkriptEventStructure.java
@@ -7,9 +7,13 @@ import ch.njol.skript.lang.Literal;
 import ch.njol.skript.lang.SkriptParser;
 import ch.njol.skript.lang.TriggerItem;
 import dev.jsinco.brewery.api.util.BreweryKey;
+import dev.jsinco.brewery.bukkit.api.BukkitAdapter;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.UnknownNullability;
 import org.skriptlang.skript.lang.entry.EntryContainer;
@@ -18,6 +22,7 @@ import org.skriptlang.skript.registration.DefaultSyntaxInfos;
 import org.skriptlang.skript.registration.SyntaxRegistry;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 public class TbpSkriptEventStructure extends Structure {
 
@@ -26,6 +31,11 @@ public class TbpSkriptEventStructure extends Structure {
     private Component displayName;
     private SectionNode source;
     private List<TriggerItem> code;
+    private final Consumer<SkriptIntegration.SkriptIntegrationEvent> completedConsumer;
+
+    public TbpSkriptEventStructure(Consumer<SkriptIntegration.SkriptIntegrationEvent> completedConsumer) {
+        this.completedConsumer = completedConsumer;
+    }
 
     @Override
     public String toString(@Nullable Event event, boolean debug) {
@@ -50,19 +60,49 @@ public class TbpSkriptEventStructure extends Structure {
     @Override
     public boolean load() {
         this.code = ScriptLoader.loadItems(source);
+        completedConsumer.accept(toTbpEvent());
         return true;
     }
 
-    public void run() {
-        TriggerItem.walk(code.getFirst(), null);
+    public SkriptIntegration.SkriptIntegrationEvent toTbpEvent() {
+        return new SkriptIntegration.SkriptIntegrationEvent(
+                key,
+                displayName,
+                player -> BukkitAdapter.toPlayer(player).ifPresent(bukkitPlayer ->
+                        TriggerItem.walk(code.getFirst(), new DummyEvent(bukkitPlayer))
+                )
+        );
     }
 
-    public static void register(SyntaxRegistry syntaxRegistry) {
+    public static void register(SyntaxRegistry syntaxRegistry, Consumer<SkriptIntegration.SkriptIntegrationEvent> completedConsumer) {
         syntaxRegistry
                 .register(SyntaxRegistry.STRUCTURE, DefaultSyntaxInfos.Structure.builder(TbpSkriptEventStructure.class)
                         .addPattern("[tbp_]register event with key %text% [and ][display ]name %text component%")
-                        .supplier(TbpSkriptEventStructure::new)
+                        .supplier(() -> new TbpSkriptEventStructure(completedConsumer))
                         .build()
                 );
+    }
+
+    public static class DummyEvent extends Event {
+        private static final HandlerList HANDLERS = new HandlerList();
+
+        private final Player player;
+
+        public DummyEvent(Player player) {
+            this.player = player;
+        }
+
+        public Player getPlayer() {
+            return this.player;
+        }
+
+        @Override
+        public @NotNull HandlerList getHandlers() {
+            return HANDLERS;
+        }
+
+        public static HandlerList getHandlerList() {
+            return HANDLERS;
+        }
     }
 }
