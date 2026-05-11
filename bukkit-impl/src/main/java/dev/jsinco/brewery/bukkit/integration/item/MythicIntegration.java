cebda017ee778d2bf1e53494e0457e0bb900d6c2
Hjalmar Gunnarsson <officialhjalmar.gunnarsson@outlook.com>
2026-05-11 10:26:47 +0200

    fix(integration): Nullability checks due to potential concurrency issues - Mythic

--- bukkit-impl/src/main/java/dev/jsinco/brewery/bukkit/integration/item/MythicIntegration.java
+++ bukkit-impl/src/main/java/dev/jsinco/brewery/bukkit/integration/item/MythicIntegration.java
@@ -14,6 +14,7 @@ import org.bukkit.inventory.ItemStack;
 import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 
+import java.util.Collection;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
@@ -76,7 +77,11 @@ public class MythicIntegration implements ItemIntegration, Listener {
     public void onEnable() {
         Bukkit.getAsyncScheduler().runAtFixedRate(TheBrewingProject.getInstance(), task -> {
             ItemManager manager = MythicBukkit.inst().getItemManager();
-            if (manager != null && !manager.getItems().isEmpty()) {
+            if (manager == null) {
+                return;
+            }
+            Collection<MythicItem> items = manager.getItems();
+            if (items != null && !items.isEmpty()) {
                 initialized.complete(null);
                 task.cancel();
             }
